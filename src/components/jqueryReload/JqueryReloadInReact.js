e6df14948ed44aac9761cd7dddd23a23f5d3f5c5
Taium <67744024+Taium@users.noreply.github.com>
2021-03-15 17:41:06 +0600

    3rd

---
+++ src/components/jqueryReload/JqueryReloadInReact.js
@@ -0,0 +1,10 @@
+//Script main js auto load useeffect
+export const MainJqueyLoad = () => {
+    const script = document.createElement('script');
+    script.src = "assets/js/main.js";
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+        document.body.removeChild(script);
+    }
+}
